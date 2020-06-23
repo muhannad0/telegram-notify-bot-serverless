3ff98c74d391cdd7795676cc4afa47dac6e925fe
Mohamed Muhannad <m.muhannad.h@gmail.com>
2020-06-24 04:34:01 +0500

    fix: localized testing (wip)

--- test/bot.test.js
+++ test/bot.test.js
@@ -1,37 +1,45 @@
 const TelegramServer = require('telegram-test-api');
-const { bot } = require('../bot')
+const { Telegraf } = require('telegraf');
+// const bot = require('../bot');
+// const { bot } = require('../bot')
 
-test('check for bot object', () => {
-    expect(typeof bot).toBe('object');
-    expect(bot).toMatchObject({ telegram: {} });
-})
+// test('check for bot object', () => {
+//     expect(typeof bot).toBe('object');
+//     expect(bot).toMatchObject({ telegram: {} });
+// })
 
 describe('testing for bot commands', () => {
     // referring to example https://www.npmjs.com/package/telegram-test-api
-    let serverConfig = {port: 9001};
+    const serverConfig = { port: 9000 };
     const token = 'sampleToken';
     let server;
-    let client;
-    
+    let bot;
     beforeAll(() => {
         server = new TelegramServer(serverConfig)
-        server.start().then(() => {
-            client = server.getClient(token)
-        })
-        await bot.startWebhook('http://localhost', null, 9001)
-        
-    })
+        return server.start().then(() => {
+            bot = new Telegraf(token, { telegram: { apiRoot: server.ApiURL } });
+            bot.start((ctx) => ctx.reply('Welcome'));
+            bot.startPolling();
+        }).then(() => {
+            return client = server.getClient(token, { timeout: 5000 });
+        });
+    });
 
     afterAll(() => {
-        this.slow(2000);
-        this.timeout(10000);
-        return server.stop();
-        return bot.stop()
-    })
+        return bot.stop().then(() => server.stop());
+    });
 
-    test('check for /start command', async () => {
-        var res = await testApi.sendMessageWithText('/help')
-        console.log(res.data)
-        // expect(typeof res.data.).toBe('string')
-    })
+    test('check if client sent command', async () => {
+        const message = client.makeCommand('/start');
+        const result = await client.sendCommand(message);
+        // console.log('result: ', result);
+        expect(result.ok).toBe(true);
+    });
+
+    test('check if bot replied', async () => {
+        const updates = await client.getUpdates();
+        // console.log(`Client received messages: ${JSON.stringify(updates.result)}`);
+        expect(updates.result.length).toBe(1);
+        expect(updates.result[0].message.text).toBe('Welcome');
+    });
 })
