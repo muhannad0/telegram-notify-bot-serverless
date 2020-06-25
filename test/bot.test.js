81fe39ac13b2be8995c7663863517e00d9dfcae5
Mohamed Muhannad <m.muhannad.h@gmail.com>
2020-06-25 14:53:48 +0500

    feat: tests for bot commands

--- test/bot.test.js
+++ test/bot.test.js
@@ -1,24 +1,29 @@
 const TelegramServer = require('telegram-test-api');
-const { Telegraf } = require('telegraf');
-// const bot = require('../bot');
-// const { bot } = require('../bot')
+// const { Telegraf } = require('telegraf');
+process.env['BOT_TOKEN'] = 'sampleToken';
+const { bot } = require('../bot')
 
-// test('check for bot object', () => {
-//     expect(typeof bot).toBe('object');
-//     expect(bot).toMatchObject({ telegram: {} });
-// })
+test('check if module returns Telegraf bot object', () => {
+    expect(typeof bot).toBe('object');
+    expect(bot).toMatchObject({ options: {}, telegram: {}, context: {}, polling: {} });
+})
 
 describe('testing for bot commands', () => {
     // referring to example https://www.npmjs.com/package/telegram-test-api
     const serverConfig = { port: 9000 };
-    const token = 'sampleToken';
+    const token = process.env.BOT_TOKEN;
     let server;
-    let bot;
+    // let bot;
     beforeAll(() => {
         server = new TelegramServer(serverConfig)
         return server.start().then(() => {
-            bot = new Telegraf(token, { telegram: { apiRoot: server.ApiURL } });
-            bot.start((ctx) => ctx.reply('Welcome'));
+            // bot = new Telegraf(token, { telegram: { apiRoot: server.ApiURL } });
+
+            // override default object options with test params
+            bot.telegram.options.apiRoot = server.ApiURL;
+            bot.telegram.options.agent = null;
+            // console.log(JSON.stringify(bot));
+
             bot.startPolling();
         }).then(() => {
             return client = server.getClient(token, { timeout: 5000 });
@@ -27,19 +32,29 @@ describe('testing for bot commands', () => {
 
     afterAll(() => {
         return bot.stop().then(() => server.stop());
+        // return server.stop();
     });
 
-    test('check if client sent command', async () => {
+    test('check if client can sent command to local api server', async () => {
         const message = client.makeCommand('/start');
         const result = await client.sendCommand(message);
         // console.log('result: ', result);
         expect(result.ok).toBe(true);
     });
 
-    test('check if bot replied', async () => {
+    test('check if replies to /start command', async () => {
         const updates = await client.getUpdates();
         // console.log(`Client received messages: ${JSON.stringify(updates.result)}`);
         expect(updates.result.length).toBe(1);
-        expect(updates.result[0].message.text).toBe('Welcome');
+        expect(updates.result[0].message.text).toMatch(new RegExp('Hello from Lambda'));
+    });
+
+    test('check if replies to /ping command', async () => {
+        const message = client.makeCommand('/ping');
+        const result = await client.sendCommand(message);
+        const updates = await client.getUpdates();
+        // console.log(`Client received messages: ${JSON.stringify(updates.result)}`);
+        expect(updates.result.length).toBe(1);
+        expect(updates.result[0].message.text).toMatch(new RegExp('Pong!'));
     });
 })
